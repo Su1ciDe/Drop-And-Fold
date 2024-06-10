6c692e5815539fed30ae7e320e21cd91ecffbbb6
Murat Öz <murat.oz@fibergames.com.tr>
2024-06-10 11:54:44 +0300

    Added sdk IDs

--- Assets/Elephant/Core/ElephantThirdPartyIds.cs
+++ Assets/Elephant/Core/ElephantThirdPartyIds.cs
@@ -2,11 +2,11 @@ namespace ElephantSDK
 {
 	public class ElephantThirdPartyIds
 	{
-		public static string FacebookClientToken = "[TEMP_GAMEKIT_FacebookClientToken]";
-		public static string FacebookAppId = "[TEMP_GAMEKIT_FacebookAppId]";
-		public static string GameId = "[TEMP_GAMEKIT_GameId]";
-		public static string GameSecret = "[TEMP_GAMEKIT_GameSecret]";
-		public static string BundleName = "[TEMP_GAMEKIT_BundleName]";
-		public static string AdjustAppKey = "[TEMP_GAMEKIT_AdjustAppKey]";
+		public static string FacebookClientToken = "94cacb417e942af3a3317127a47db38d";
+		public static string FacebookAppId = "341954448708705";
+		public static string GameId = "725635224950208578760493d4124e8114e069111863a61ad18d01ae8befaeaf";
+		public static string GameSecret = "c63064ae6c72629cec5bb57404bf2b10297f5dc564553619ef4c49c37a312fcb";
+		public static string BundleName = "com.FiberGames.DropAndFold";
+		public static string AdjustAppKey = "d9j8qjkkrsw0";
 	}
 }
--- Assets/RollicGames/RollicApplovinIDs.cs
+++ Assets/RollicGames/RollicApplovinIDs.cs
@@ -2,14 +2,14 @@ namespace RollicGames.Advertisements
 {
 	public class RollicApplovinIDs
 	{
-		public static string AppKey = "[TEMP_GAMEKIT_AppKey]";
-		public static string BannerAdUnitIos = "[TEMP_GAMEKIT_BannerAdUnitIos]";
-		public static string InterstitialAdUnitIos = "[TEMP_GAMEKIT_InterstitialAdUnitIos]";
-		public static string RewardedAdUnitIos = "[TEMP_GAMEKIT_RewardedAdUnitIos]";
-		public static string BannerAdUnitAndroid = "[TEMP_GAMEKIT_BannerAdUnitAndroid]";
-		public static string InterstitialAdUnitAndroid = "[TEMP_GAMEKIT_InterstitialAdUnitAndroid]";
-		public static string RewardedAdUnitAndroid = "[TEMP_GAMEKIT_RewardedAdUnitAndroid]";
-		public static string GoogleIosId = "[TEMP_GAMEKIT_GoogleAppIdIos]";
-		public static string GoogleAndroidId = "[TEMP_GAMEKIT_GoogleAppIdAndroid]";
+		public static string AppKey = "HXfHkhyBTH16scCO01rE3sKd5GHRl9bNlMnsETWk9y2R_SsH6y6m5N3HrWutctNv04y0EIdil5zJu0FMT47KOQ";
+		public static string BannerAdUnitIos = "d6249ece539bad45";
+		public static string InterstitialAdUnitIos = "932de1ec8cf3cd05";
+		public static string RewardedAdUnitIos = "78d2b0351b9982cb";
+		public static string BannerAdUnitAndroid = "0213862705911b28";
+		public static string InterstitialAdUnitAndroid = "57118f69520c74cb";
+		public static string RewardedAdUnitAndroid = "13f07c2796fe6572";
+		public static string GoogleIosId = "";
+		public static string GoogleAndroidId = "";
 	}
 }
